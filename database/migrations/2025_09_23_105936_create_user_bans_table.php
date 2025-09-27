4df129903b041eefc252e53e491c078e61dcad97
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-09-27 11:00:05 +0300

    feat: add `user_bans` table

---
+++ database/migrations/2025_09_23_105936_create_user_bans_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user_bans', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->text('reason');
+            $table->timestamp('banned_at')->useCurrent();
+            $table->timestamp('expires_at')->nullable();
+            $table->foreignIdFor(User::class, 'banned_by_user_id')->constrained();
+            $table->boolean('is_active')->default(true);
+            $table->text('notes')->nullable();
+            $table->timestamps();
+
+            $table->index(['user_id', 'is_active']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user_bans');
+    }
+};
