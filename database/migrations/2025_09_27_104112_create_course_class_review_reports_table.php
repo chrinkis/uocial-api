d4c14bba45db89fa991055844872b9cb926c1486
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-09-27 13:54:37 +0300

    feat: add `course_class_review_reports` table

---
+++ database/migrations/2025_09_27_104112_create_course_class_review_reports_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\CourseClassReview;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('course_class_review_reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class)->constrained();
+            $table->foreignIdFor(CourseClassReview::class)->constrained();
+            $table->text('user_comment');
+            $table->foreignIdFor(User::class, 'reviewed_by_user_id')->nullable()->constrained();
+            $table->timestamp('reviewed_at')->nullable();
+            $table->text('reviewer_notes')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('course_class_review_reports');
+    }
+};
