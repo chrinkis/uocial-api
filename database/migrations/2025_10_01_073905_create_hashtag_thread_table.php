34b36a373f781fc5f68080738c2e24fd961466a4
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-01 10:41:04 +0300

    feat: add `hashtag_thread` table

---
+++ database/migrations/2025_10_01_073905_create_hashtag_thread_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Hashtag;
+use App\Models\Thread;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('hashtag_thread', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Thread::class)->constrained();
+            $table->foreignIdFor(Hashtag::class)->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('hashtag_thread');
+    }
+};
