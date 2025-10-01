58429fb0f92c3679033a54d964420d50a741c357
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-01 10:48:13 +0300

    feat: add `thread_comment` table

---
+++ database/migrations/2025_10_01_074456_create_thread_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Models\Thread;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('thread_comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Thread::class)->constrained();
+            $table->foreignIdFor(User::class);
+            $table->text('comment');
+            $table->foreignIdFor(User::class, 'hidden_by_user_id')->nullable()->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('thread_comments');
+    }
+};
