586c1863ebbd014ca6da3f9732b2f92b42a13d99
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-04 19:50:08 +0300

    feat: add `UserSeeder`

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()
+            ->unverified()
+            ->state(['email' => 'unverified@uoc.gr'])
+            ->create();
+
+        User::factory()
+            ->state(['email' => 'verified@uoc.gr'])
+            ->create();
+
+        User::factory()
+            ->state(['email' => 'regular@uoc.gr'])
+            ->create();
+
+        User::factory()
+            ->moderator()
+            ->state(['email' => 'moderator@uoc.gr'])
+            ->create();
+
+        User::factory()
+            ->admin()
+            ->state(['email' => 'admin@uoc.gr'])
+            ->create();
+
+    }
+}
