26eeecb8ddc8756b72a27e4b2b5b74277cf2579a
Alexandros Christinakis <csd4764@csd.uoc.gr>
2025-10-11 17:37:26 +0300

    feat: add post seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $user_id = User::regularVerified()
+            ->inRandomOrder()
+            ->value('id');
+
+        return [
+            'user_id' => $user_id,
+            'title' => fake()->sentence(rand(1, 5)),
+            'body' => fake()->text(rand(12, 1023)),
+        ];
+    }
+
+    /**
+     * Indicate that the post should be hidden.
+     */
+    public function hidden(): static
+    {
+        $moderator_id = User::moderators()
+            ->inRandomOrder()
+            ->value('id');
+
+        return $this->state(fn (array $attributes) => [
+            'hidden_by' => $moderator_id,
+            'moderator_comment' => fake()->sentence(rand(3, 8)),
+        ]);
+    }
+}
---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+use Illuminate\Database\Seeder;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Post::factory()
+            ->count(32)
+            ->create();
+
+        Post::factory()
+            ->hidden()
+            ->count(32)
+            ->create();
+    }
+}
